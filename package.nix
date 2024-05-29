b404d0fc8e304284d7fc8c16e1c5c517b0d4e970
RGBCube <git@rgbcu.be>
2024-05-29 10:01:10 +0300

    Add package.nix

---
+++ package.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenvNoCC,
+
+  nushell,
+}:
+
+stdenvNoCC.mkDerivation {
+  name    = "github2forgejo";
+  version = "master";
+
+  src = ./.;
+
+  dontBuild     = true;
+  dontConfigure = true;
+
+  nativeBuildInputs = [ nushell ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp github2forgejo $out/bin
+
+    runHook postInstall
+  '';
+
+  fixupPhase = ''
+    runHook preFixup
+
+    substituteInPlace $out/bin/github2forgejo \
+      --replace '/usr/bin/env nu' '${nushell}/bin/nu'
+
+    runHook postFixup
+  '';
+
+  meta = with lib; {
+    description = "GitHub to Forgejo migration script";
+    homepage    = "https://git.rgbcu.be/RGBCube/GitHub2Forgejo";
+    license     = licenses.gpl3Only;
+    maintainers = with maintainers; [ RGBCube ];
+  };
+}
